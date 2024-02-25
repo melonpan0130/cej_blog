b0dd08eab86c144079a4a5f66da7db3b5a615418
melonpan0130 <eun01300714@gmail.com>
2024-02-25 17:24:26 +0900

    Utterances

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -7,11 +7,31 @@ const fileName = route.params.fileName.toString();
 
 axios.get(`/posts/${fileName}`)
   .then((res)=> {
+    // Set content
     document.getElementById('PostComponent')!.innerHTML = res.data;
+
+    // Set Utterances
+    const utterances = document.createElement('script');
+    utterances.type = 'text/javascript';
+    utterances.async = true;
+    utterances.src = 'https://utteranc.es/client.js';
+    utterances.setAttribute('crossorigin', 'anonymous')
+    utterances.setAttribute('issue-term', 'pathname');
+    utterances.setAttribute('theme', 'github-light');
+    utterances.setAttribute('repo', 'melonpan0130/cej_blog');
+    document.getElementById('utterances')!.appendChild(utterances);
   })
   .catch(err=>console.log(err));
 </script>
 
+
 <template>
   <div id="PostComponent"></div>
+  <div id="utterances"></div>
 </template>
+
+<style>
+#PostComponent {
+  border-bottom: solid 1px #aeaeae;
+}
+</style>
