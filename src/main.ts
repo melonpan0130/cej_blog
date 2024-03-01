a840ef6231ab637c4bedb5e532360c1ef4364ab7
melonpan0130 <eun01300714@gmail.com>
2024-03-01 21:54:36 +0900

    test2

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
-import { createApp } from 'vue'
+import { createSSRApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
 import posts from './posts.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createSSRApp(App)
 
 app.config.globalProperties.$posts = posts;
 app.use(router);
