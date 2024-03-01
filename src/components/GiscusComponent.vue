c78b5ed2c4cb41940ef75eb84ae9ae19553bebd5
melonpan0130 <eun01300714@gmail.com>
2024-03-02 03:10:51 +0900

    Giscus(3)

---
+++ src/components/GiscusComponent.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+export default {
+  mounted() {
+    // Set Giscus
+    const giscus = document.createElement('script');
+    giscus.src ='https://giscus.app/client.js';
+    giscus.async = true;
+    giscus.setAttribute('data-repo', 'melonpan0130/cej_blog');
+    giscus.setAttribute('data-repo-id', 'R_kgDOLXW8iQ');
+    giscus.setAttribute('data-category-id', 'DIC_kwDOLXW8ic4CdpHp');
+    giscus.setAttribute('data-mapping', 'pathname');
+    giscus.setAttribute('data-strict', '0');
+    giscus.setAttribute('data-reactions-enabled', '1');
+    giscus.setAttribute('data-emit-metadata', '0');
+    giscus.setAttribute('data-input-position', 'bottom');
+    giscus.setAttribute('data-theme', 'preferred_color_scheme');
+    giscus.setAttribute('data-lang', 'ko');
+    giscus.setAttribute('crossorigin', 'anonymous');
+    document.getElementById('giscus')?.appendChild(giscus);
+  }
+}
+</script>
+
+<template>
+  <div id="giscus"></div>
+</template>
--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import axios from 'axios';
 import { useRoute } from 'vue-router';
+import GiscusComponent from './GiscusComponent.vue';
 
 const route = useRoute();
 const fileName:string[] = route.params.fileName as string[];
@@ -9,30 +10,13 @@ axios.get(`/posts/${fileName.join('/')}.html`)
   .then((res)=> {
     // Set content
     document.getElementById('PostComponent')!.innerHTML = res.data;
-
-    // Set Giscus
-    const giscus = document.createElement('script');
-    giscus.src ='https://giscus.app/client.js';
-    giscus.async = true;
-    giscus.setAttribute('data-repo', 'melonpan0130/cej_blog');
-    giscus.setAttribute('data-repo-id', 'R_kgDOLXW8iQ');
-    giscus.setAttribute('data-category-id', 'DIC_kwDOLXW8ic4CdpHp');
-    giscus.setAttribute('data-mapping', 'pathname');
-    giscus.setAttribute('data-strict', '0');
-    giscus.setAttribute('data-reactions-enabled', '1');
-    giscus.setAttribute('data-emit-metadata', '0');
-    giscus.setAttribute('data-input-position', 'bottom');
-    giscus.setAttribute('data-theme', 'preferred_color_scheme');
-    giscus.setAttribute('data-lang', 'ko');
-    giscus.setAttribute('crossorigin', 'anonymous');
-    document.getElementById('giscus')?.appendChild(giscus);
   })
   .catch(err=>console.log(err));
 </script>
 
 <template>
   <div id="PostComponent"></div>
-  <div id="giscus"></div>
+  <GiscusComponent />
 </template>
 
 <style>
